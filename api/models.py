858808c82186d0a5e33f4a7e1c7cfa70f2c3ba6c
Verde888 <michaljzielinski@gmail.com>
2022-01-27 21:58:53 +0100

    minor-change

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=400)
     completed = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
