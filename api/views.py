e02f382d669198460834ae215a7f1fb9bb29393f
Verde888 <michaljzielinski@gmail.com>
2022-01-29 17:32:39 +0100

    new-commit

--- api/views.py
+++ api/views.py
@@ -44,3 +44,22 @@ def task_create(request):
         serializer.save()
 
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response(f"Item ({task.title}) successfully deleted")
