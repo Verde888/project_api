145ebc0e58f86e6efd9703b1e2e5c33823bccd5e
Verde888 <michaljzielinski@gmail.com>
2022-01-29 16:54:01 +0100

    new-commit

--- api/views.py
+++ api/views.py
@@ -22,7 +22,25 @@ def api_overview(request):
     return Response(api_urls)
 
 
+@api_view(["GET"])
 def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def task_detail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
     return Response(serializer.data)
